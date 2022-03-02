e0a4be437c5a8f1275f0a6d21946e1f115ed3ad9
Sara Maradiago <70499799+SJMC29@users.noreply.github.com>
2022-03-01 22:15:15 -0800

    Pez individual y estúpido

--- Assets/Scripts/FishController.cs
+++ Assets/Scripts/FishController.cs
@@ -6,15 +6,18 @@ using UnityEngine.Rendering.Universal;
 public class FishController : MonoBehaviour
 {
   Vector2 mousePosition;
-  Vector2 direction;
+  Vector2 direction = new Vector2(5, 3);
   float angle;
   float impulse;
   public float turnSpeed;
   public float moveSpeed;
+  public float fishMemory;
   Rigidbody2D rb;
+  private bool rotar = true;   
   //public Transform playerLight;
   public Light2D lt;
   // Start is called before the first frame update
+
   void Start()
   {
     rb = GetComponent<Rigidbody2D>();
@@ -23,24 +26,43 @@ public class FishController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+        
+        if (rotar)
+        {
+            rotar = false;
+            StartCoroutine("changeDirection");
+        }
 
-    // Get world position for the mouse
-    mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    // Get the direction of the mouse relative to the player and rotate the player to said direction
-    direction = mousePosition - (Vector2)transform.position;
-    angle = Vector2.SignedAngle(transform.right, direction);
+
+        // Get world position for the mouse
+        //position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Get the direction of the mouse relative to the player and rotate the player to said direction
+        //direction = position - (Vector2)transform.position;
+
+        //direction = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));    
+
+        angle = Vector2.SignedAngle(transform.right, direction);
     impulse = angle * Mathf.Deg2Rad * turnSpeed * rb.inertia;
     rb.AddTorque(impulse, ForceMode2D.Force);
 
-    if (Input.GetKey(KeyCode.Mouse1))
-    {
-      rb.AddForce((Vector2)transform.right * moveSpeed * Time.deltaTime);
-    }
+    rb.AddForce((Vector2)transform.right * moveSpeed * Time.deltaTime);
+    
     //playerLight.transform.position = transform.position;
-    lt.intensity = 0.3f + Mathf.PingPong(Time.time / 2, 0.7f);
+    //lt.intensity = 0.3f + Mathf.PingPong(Time.time / 2, 0.7f);
   }
 
-  //private void OnTriggerEnter2D(Collider2D collision){
-  //  Destroy(gameObject);
-  //}
+  IEnumerator changeDirection()
+  {        
+        //Wait for fishMemory seconds
+        yield return new WaitForSeconds(fishMemory);
+        Debug.Log("Cambié la dirección: ");
+        direction = new Vector2(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
+
+        rotar = true;
+
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision){
+    //  Destroy(gameObject);
+    //}
 }
